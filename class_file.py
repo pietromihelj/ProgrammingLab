da730ff7a1325901bf93592e0768779219593620
pietromihelj <115544486+pietromihelj@users.noreply.github.com>
2022-11-24 13:31:57 +0000

    controllo file inesistenti

--- class_file.py
+++ class_file.py
@@ -2,8 +2,8 @@ class CSVFile():
     def __init__(self,name):
         self.name = name
         try: 
-            opne(self.name,'r')
-        except NameError:
+            open(self.name,'r')
+        except:
             print('Errore')
 
     def get_data(self):
@@ -27,7 +27,9 @@ class CSVFile():
             
 
         return data
-            
+
+
+
 
                 
 
